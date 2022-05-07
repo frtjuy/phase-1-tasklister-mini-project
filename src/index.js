19108498459396899578fbf461230caeff98b856
Alexander Edwards <alexd.edwards1999@gmail.com>
2022-05-07 05:18:43 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+const descript = document.getElementById('new-task-description');
+const form = document.getElementById('create-task-form');
+const submits = document.getElementById('submit-button');
+const task = document.getElementById('tasks');
+const todo = [];
+function undefaultForm() {
+  form.addEventListener('click', function(event) {
+    event.preventDefault()
+  })
+}
+function addStuff() {
+  const li = document.createElement('li')
+  todo.forEach(function(element) {
+    task.appendChild(li)
+    li.textContent = element
+  })
+}
+function addToDos() {
+  submits.addEventListener('click', function (event) {
+    if (event) {
+      todo.push(descript.value)
+      addStuff();
+      descript.value = ""
+    }
+  
+  })
+}
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  undefaultForm();
+  addToDos();
 });
